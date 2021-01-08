b100ed64ab59a027d451463c220a1adc286e191a
Emin Hayal <eminhayal768@gmail.com>
2021-01-09 00:16:43 +0300

    Uygulamanın bitmiş hali

---
+++ main/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace main
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.game = new System.Windows.Forms.PictureBox();
+            this.score_label = new System.Windows.Forms.Label();
+            this.timer_label = new System.Windows.Forms.Label();
+            this.timer_score = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKişileriKaydet = new System.Windows.Forms.TextBox();
+            this.btnSkorlarıGörüntüle = new System.Windows.Forms.Button();
+            this.btnKişiyikaydet = new System.Windows.Forms.Button();
+            this.btnYardım = new System.Windows.Forms.Button();
+            this.timer_time = new System.Windows.Forms.Timer(this.components);
+            this.bilgiPanel = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kolayRdb = new System.Windows.Forms.RadioButton();
+            this.zorRdb = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.game)).BeginInit();
+            this.bilgiPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // game
+            // 
+            this.game.AccessibleDescription = "";
+            this.game.BackColor = System.Drawing.Color.GhostWhite;
+            this.game.Location = new System.Drawing.Point(91, 78);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(500, 500);
+            this.game.TabIndex = 0;
+            this.game.TabStop = false;
+            // 
+            // score_label
+            // 
+            this.score_label.AutoSize = true;
+            this.score_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.score_label.Location = new System.Drawing.Point(556, 35);
+            this.score_label.Name = "score_label";
+            this.score_label.Size = new System.Drawing.Size(42, 20);
+            this.score_label.TabIndex = 1;
+            this.score_label.Text = "Skor";
+            // 
+            // timer_label
+            // 
+            this.timer_label.AutoSize = true;
+            this.timer_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.timer_label.Location = new System.Drawing.Point(166, 30);
+            this.timer_label.Name = "timer_label";
+            this.timer_label.Size = new System.Drawing.Size(51, 20);
+            this.timer_label.TabIndex = 2;
+            this.timer_label.Text = "label1";
+            // 
+            // timer_score
+            // 
+            this.timer_score.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(258, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yilan Oyunu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(491, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Puan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(43, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Geçen Süre:";
+            // 
+            // txtKişileriKaydet
+            // 
+            this.txtKişileriKaydet.Location = new System.Drawing.Point(231, 626);
+            this.txtKişileriKaydet.Name = "txtKişileriKaydet";
+            this.txtKişileriKaydet.Size = new System.Drawing.Size(113, 20);
+            this.txtKişileriKaydet.TabIndex = 13;
+            this.txtKişileriKaydet.TabStop = false;
+            // 
+            // btnSkorlarıGörüntüle
+            // 
+            this.btnSkorlarıGörüntüle.Location = new System.Drawing.Point(560, 653);
+            this.btnSkorlarıGörüntüle.Name = "btnSkorlarıGörüntüle";
+            this.btnSkorlarıGörüntüle.Size = new System.Drawing.Size(116, 20);
+            this.btnSkorlarıGörüntüle.TabIndex = 12;
+            this.btnSkorlarıGörüntüle.TabStop = false;
+            this.btnSkorlarıGörüntüle.Text = "Skorları Görüntüle";
+            this.btnSkorlarıGörüntüle.UseVisualStyleBackColor = true;
+            this.btnSkorlarıGörüntüle.Click += new System.EventHandler(this.btnSkorlarıGörüntüle_Click);
+            // 
+            // btnKişiyikaydet
+            // 
+            this.btnKişiyikaydet.Location = new System.Drawing.Point(350, 626);
+            this.btnKişiyikaydet.Name = "btnKişiyikaydet";
+            this.btnKişiyikaydet.Size = new System.Drawing.Size(113, 20);
+            this.btnKişiyikaydet.TabIndex = 11;
+            this.btnKişiyikaydet.TabStop = false;
+            this.btnKişiyikaydet.Text = "Kişiyi Kaydet";
+            this.btnKişiyikaydet.UseVisualStyleBackColor = true;
+            this.btnKişiyikaydet.Click += new System.EventHandler(this.btnKişiyikaydet_Click);
+            // 
+            // btnYardım
+            // 
+            this.btnYardım.Location = new System.Drawing.Point(27, 653);
+            this.btnYardım.Name = "btnYardım";
+            this.btnYardım.Size = new System.Drawing.Size(115, 20);
+            this.btnYardım.TabIndex = 10;
+            this.btnYardım.TabStop = false;
+            this.btnYardım.Text = "Yardım";
+            this.btnYardım.UseVisualStyleBackColor = true;
+            this.btnYardım.Click += new System.EventHandler(this.btnYardım_Click);
+            // 
+            // timer_time
+            // 
+            this.timer_time.Tick += new System.EventHandler(this.timer_time_Tick);
+            // 
+            // bilgiPanel
+            // 
+            this.bilgiPanel.BackColor = System.Drawing.Color.Lavender;
+            this.bilgiPanel.Controls.Add(this.label5);
+            this.bilgiPanel.Location = new System.Drawing.Point(91, 78);
+            this.bilgiPanel.Name = "bilgiPanel";
+            this.bilgiPanel.Size = new System.Drawing.Size(500, 500);
+            this.bilgiPanel.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(72, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(355, 199);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Başlamak için [B] tuşuna , durdurmak için [D] tuşuna basınız.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(625, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // kolayRdb
+            // 
+            this.kolayRdb.AutoSize = true;
+            this.kolayRdb.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kolayRdb.Location = new System.Drawing.Point(293, 584);
+            this.kolayRdb.Name = "kolayRdb";
+            this.kolayRdb.Size = new System.Drawing.Size(51, 17);
+            this.kolayRdb.TabIndex = 16;
+            this.kolayRdb.TabStop = true;
+            this.kolayRdb.Text = "Kolay";
+            this.kolayRdb.UseVisualStyleBackColor = true;
+            // 
+            // zorRdb
+            // 
+            this.zorRdb.AutoSize = true;
+            this.zorRdb.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.zorRdb.Location = new System.Drawing.Point(350, 584);
+            this.zorRdb.Name = "zorRdb";
+            this.zorRdb.Size = new System.Drawing.Size(41, 17);
+            this.zorRdb.TabIndex = 17;
+            this.zorRdb.TabStop = true;
+            this.zorRdb.Text = "Zor";
+            this.zorRdb.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(700, 682);
+            this.Controls.Add(this.zorRdb);
+            this.Controls.Add(this.kolayRdb);
+            this.Controls.Add(this.bilgiPanel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKişileriKaydet);
+            this.Controls.Add(this.btnSkorlarıGörüntüle);
+            this.Controls.Add(this.btnKişiyikaydet);
+            this.Controls.Add(this.btnYardım);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timer_label);
+            this.Controls.Add(this.score_label);
+            this.Controls.Add(this.game);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.game)).EndInit();
+            this.bilgiPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox game;
+        private System.Windows.Forms.Label score_label;
+        private System.Windows.Forms.Label timer_label;
+        private System.Windows.Forms.Timer timer_score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKişileriKaydet;
+        private System.Windows.Forms.Button btnSkorlarıGörüntüle;
+        private System.Windows.Forms.Button btnKişiyikaydet;
+        private System.Windows.Forms.Button btnYardım;
+        private System.Windows.Forms.Timer timer_time;
+        private System.Windows.Forms.Panel bilgiPanel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton kolayRdb;
+        private System.Windows.Forms.RadioButton zorRdb;
+    }
+}
+
---
+++ main/Form1.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace main
+{
+    public partial class Form1 : Form
+    {
+        string kullanici = null, skor_dosya_yolu = @"C:\Users\rakio\Desktop\puan.txt", yardim_dosya_yolu = @"C:\Users\Acer\Desktop\denemeYardim.txt";
+        public Thread thread;
+
+        public enum Direction  //Yılanın hareketini yönlendirmek
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        public int x = 1, y = 1; // Kuyruk ve yılanın başlangıç konumunu ve kuyruğun oluşumun da kullanılan değişkenler.
+        public int a; // Skoru tutma değişken.  
+        
+        public int score; // Skorun hesaplanması 
+
+        public int time = 0; // Skorda kullanılan süre 
+        public int cutTime; // Skorda iki yem yeme arasındaki tutulan zaman
+        public int firstTime = 0; // Skorda oluşan ilk zaman
+
+        public double minute = 0; // Sayaçta ekrana dakikayı yazmak için kullanılır.
+        public double second = 0; // Sayaçta ekrana saniyeyi yazmak için kullanılır.
+        public double timeGame = 0; // Sayaçta kullanılan süre
+
+        public Direction direction = Direction.Right; // 
+
+        public Location food = new Location(-1, -1);// Yemeğin oluşacağı konumu belirlemek için classdan çektiğimiz method.
+
+        Random random = new Random(); // Random classından yeni bir random nesneni oluşturuyoruz.
+
+        public List<Location> tales = new List<Location>(); // Kuyruk kısmını listede tutuyoruz .
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            tales.Add(new main.Location(0, 0)); // Yeni kuyruk ekleme 
+
+            GameTable();
+
+            Control.CheckForIllegalCrossThreadCalls = false; // 
+            timer_label.Text = "0";
+            timer_score.Interval = 1000;
+            timer_time.Interval = 1000;
+
+
+            btnYardım.Enabled = true;
+            btnSkorlarıGörüntüle.Enabled = true;
+            btnKişiyikaydet.Enabled = true;
+
+            thread = new Thread(new ThreadStart(new Action(() =>
+            {
+                while (true)
+                {
+                    if (direction == Direction.Right) x++;
+                    if (direction == Direction.Down) y++;
+                    if (direction == Direction.Left) x--;
+                    if (direction == Direction.Up) y--;
+
+                    
+
+                    GameTable();
+                    if(kolayRdb.Checked == true)
+                    {
+                        Thread.Sleep(200);
+                    }
+                    else
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+
+            })));
+            thread.Start();
+            thread.Suspend(); // thread duraklatılır
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.KeyCode == Keys.Down) direction = Direction.Down; 
+            if (e.KeyCode==Keys.Left) direction = Direction.Left;
+            if (e.KeyCode==Keys.Up) direction = Direction.Up;
+            if (e.KeyCode==Keys.Right) direction = Direction.Right;
+           
+            if(e.KeyCode == Keys.B)
+            {
+
+                btnYardım.Enabled = false;
+                btnSkorlarıGörüntüle.Enabled = false;
+                btnKişiyikaydet.Enabled = false;
+
+                zorRdb.Enabled = false;
+                kolayRdb.Enabled = false;
+
+                timer_score.Enabled = true;
+                timer_time.Enabled = true;
+
+                bilgiPanel.Visible = false;
+
+
+                txtKişileriKaydet.Enabled = false;
+
+                thread.Resume(); // Resume() metodu Suspend() metodundan sonra çalışabilir.
+ 
+            }
+            // D tuşuna basıldığında olacaklar
+            if (e.KeyCode == Keys.D)
+            {
+                zorRdb.Enabled = true;
+                kolayRdb.Enabled = true;
+
+                thread.Suspend();
+
+                bilgiPanel.Visible = true;
+                timer_score.Enabled = false;
+                timer_time.Enabled = false;
+
+
+                btnYardım.Enabled = true;
+                btnSkorlarıGörüntüle.Enabled = true;
+                btnKişiyikaydet.Enabled = true;
+            } 
+        }
+        public void GameTable()
+        {
+            // Yılanın kuyruğu üst üste geldiğinde yanması
+            if (tales.Count != 1)
+            {
+                for (int i = 1; i < tales.Count; i++)
+                {   
+                    if (tales[i].x == x && tales[i].y == y)
+                    {
+
+                        DosyaIslemi();
+
+                        GameOver();
+                    }
+                }
+            }
+
+            // Yemek oluşturma
+            if (x == food.x && y == food.y)
+            {
+                tales.Add(new main.Location(food.x, food.y));
+                food = new Location(-1, -1);
+
+                //1,3 / 49,51 sol alt
+                //49,51 / 1,3 sag ust
+                // 49,51 / 49,51 sag alt 
+                // 1,3 / 1,3 sol ust
+
+                // Scoreların verilme yeri 
+         
+                if (x == 1 || y == 3 || x == 49 || y == 51) { Score(); }
+                if (x == 49 || y == 51 || x == 1 || y == 3) { Score(); }
+                if (x == 49 || y == 51 || x == 49 || y == 51) { Score(); }
+                if (x == 1 || y == 3 || x == 1 || y == 3) { Score(); }
+                else
+                {
+                    
+                    cutTime = time - firstTime;
+                    score = 100 / cutTime;
+                    firstTime = cutTime;
+                    if (cutTime > 40)
+                    {
+                        a += 0; 
+                        score_label.Text = a.ToString();
+                        
+                    }
+                    else 
+                    {   
+                        a += score;
+                        score_label.Text = a.ToString();
+                        
+                    }
+                    
+                }
+             }
+            Bitmap bitmap = new Bitmap(500, 500); // 500 * 500 lük bir bitmap oluşumu.
+
+            // köşelere çarptığında yanması 
+            if (x == 51 || y == 51 || x ==51 || y == 51)
+            {
+                timer_score.Stop();
+                DosyaIslemi();
+             
+                GameOver();
+            }
+            else if (x == 0 || y == 0 || x == 0 || y == 0)
+            {
+                timer_score.Stop();
+                DosyaIslemi();
+                GameOver();
+            }
+            else
+            {
+
+                // Yılanın baş kısmının oluşması
+                for (int i = (x - 1) * 10; i < x * 10; i++)
+                {
+                    for (int j = (y - 1) * 10; j < y * 10; j++)
+                    {
+
+                        if (x > 510 || y > 510)
+                        {
+                            
+                            i = 0;
+                            j = 0;
+                        }
+                        else
+                        {
+                            bitmap.SetPixel(i, j, Color.Green);
+
+                        }
+                      
+                        
+                    }
+                }
+            }
+
+            // Kuyruğun oluşumu 
+            if (tales.Count != 1)
+            {
+                for (int k = 0; k < tales.Count; k++)
+                {
+                    for (int i = (tales[k].x - 1) * 10; i < tales[k].x * 10; i++)
+                    {
+                        for (int j = (tales[k].y - 1) * 10; j < tales[k].y * 10; j++)
+                        {
+                            if (i < 1 || i > 510 || j < 1 || j > 510 )
+                            {
+                                
+                                i = 0;
+                                j = 0;
+                        }
+                            else
+                            {
+                                bitmap.SetPixel(i, j, Color.Green);
+
+                            }
+                            
+
+                        }
+                    }
+                }
+            }
+
+            tales[0] = new Location(x, y);
+            for (int i = tales.Count - 1; i > 0; i--)
+            {
+                tales[i] = tales[i - 1];
+            }
+
+            // Yemek gittiginde yeniden olusan yem 
+            if (food.x == -1)
+            {
+                food = new Location(random.Next(1, 51), random.Next(1, 51));
+            }
+
+            for (int i = (food.x - 1) * 10; i < food.x * 10; i++)
+            {
+                for (int j = (food.y - 1) * 10; j < food.y * 10; j++)
+                {
+
+                    bitmap.SetPixel(i, j, Color.Red);
+                    
+                }
+            }
+            game.Image = bitmap;
+        }
+
+        // Puanları tutmak için oluşturularn .txt dosyasının içeriği
+        private void DosyaIslemi()
+        {
+            StreamWriter yazici = File.AppendText(skor_dosya_yolu);
+            yazici.WriteLine(kullanici + " " + timer_label.Text.ToString() + " " + score_label.Text.ToString());
+            yazici.Close();
+        }
+
+         // Puanı tutmak için tuttuğumuz zaman 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            int a = 0;
+
+            if (time >= 59)
+            {
+                a++;
+              
+            }
+        }
+
+         // Sayacı oluşturma ve ekrana yazdırma
+        private void timer_time_Tick(object sender, EventArgs e)
+        {
+
+            timeGame++;
+            second= timeGame;
+            timer_label.Text = ((Convert.ToString(minute) + " : ") + (Convert.ToString(second)));
+            if (second == 59)
+            {
+                timeGame = 0; 
+                second = 0;
+                minute = minute + 1;
+            }
+
+        }
+        // Dosyaları kendi bilgisayarınız da uzantılarını yazarak tutmanız gerekir .txt dosyası 
+        private void btnSkorlarıGörüntüle_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", skor_dosya_yolu);
+        }
+
+        private void btnYardım_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", yardim_dosya_yolu);
+        }
+
+        private void btnKişiyikaydet_Click(object sender, EventArgs e)
+        {
+            kullanici = txtKişileriKaydet.Text.ToString();
+            MessageBox.Show("Kişi Kaydedildi!");
+        }
+
+        // Skoru hesaplamak için 
+        public void Score()
+        {
+            if (cutTime != 0)
+            {
+                
+                cutTime = time - firstTime;
+                score = (100 / cutTime) + 10;
+                firstTime = cutTime;
+                if (cutTime > 10)
+                {
+                    a += 0;
+                    score_label.Text = a.ToString();
+                    
+                }
+                else
+                {
+                    a += score;
+                    score_label.Text = a.ToString();
+                    
+                }
+                
+            }
+
+        }
+        // Oyunda yandığımızda gösterilen Mesaj 
+        public void GameOver()
+        {
+            DialogResult dialogResult = new DialogResult();
+            dialogResult = MessageBox.Show("Yeniden oynamak ister misiniz ? ", "Kaybettiniz", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        // oyunun yeniden baslamasi 
+        public void restart()
+        {
+            Form1 startForm = new Form1();
+            this.Hide();
+            startForm.Show();
+
+            Application.Restart();
+            
+        }
+
+    }
+    
+    // yemi ve kuyruğun konumunu tumak için
+    public class Location
+    {
+        public int x, y;
+
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+}
